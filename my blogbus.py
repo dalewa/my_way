4754af06e9c18f680dfd7fdca250ba8568902a03
dalewa <dale0421@gmail.com>
2017-05-29 17:35:30 +0800

    适用Py3且写入txt

--- my blogbus.py
+++ my blogbus.py
@@ -1,29 +1,24 @@
 # -*- coding:utf-8 -*-
-import urllib
-import urllib2
+import requests
 import re
 
-page = range(1,2)
+page = range(1, 2)
+data_all = ''
 for i in page:
-    url = 'http://dalewa.blogbus.com/index_' + str(i) + '.html'
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-    headers = {'User-Agent': user_agent}
-    try:
-        request = urllib2.Request(url, headers = headers)
-        response = urllib2.urlopen(request)
-        # print response.read()
-        content = response.read().decode('utf-8')
-        pattern=re.compile('h2.*?.html">(.*?)</a>.*?'+
-                           '<h5 class="date">(.*?)</h5>.*?' +
-                           '<div class="postBody">(.*?)</div> *?',re.S)
-        items = re.findall(pattern, content)
-        for item in items:
-            li =  item[0].strip() + '\t' + item[1].strip() +  '\n' +  item[2].strip() +  '\n'
-            print li
-        
+    url = 'http://dalewa.blogbus.com/index_%d.html' % i
+    req = requests.get(url)
+    html = req.text
 
-    except urllib2.URLError, e:
-        if hasattr(e, 'code'):
-            print e.code
-        if hasattr(e, 'reason'):
-            print e.reason
+    pattern = re.compile('h2.*?.html">(.*?)</a>.*?'+
+                           '<h5 class="date">(.*?)</h5>.*?' +
+                        '<div class="postBody">(.*?)</div> *?',re.S)
+    items = re.findall(pattern,html)
+    for item in items:
+        li = item[0].strip() + '\t' + item[1].strip() + '\n' + item[2].strip() + '\n\n'
+        li = li.replace('<p>','')
+        li = li.replace('</p>', '')
+        li = li.replace('<br /><br />', '\n')
+        data_all += li
+
+with open('myblog.txt', 'w', encoding = 'utf-8')  as f:
+    f.write(data_all)
